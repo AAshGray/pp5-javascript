18c8bc036d47010c9e20216963984d2084bdfa69
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-01-23 13:04:53 -0800

    added the password requirements to the scripts file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,9 +4,19 @@ document.getElementById('click').onclick = function()
     let outputMessages = ""
     let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*']
     
+    // Password Requirements
+    // Minimum length: 8
+    // At least one special character (from specialCharacters array)
+    // no spaces
+    // At least 1 uppercase letter
+    // At least 1 lowercase letter
+    // At least 1 numeric digit
+
     if (password.length < 8) {
         outputMessages += "The password must be at least 8 characters long.";
     }
+
+    
 
     
     
