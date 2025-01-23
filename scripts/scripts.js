4824bc4f4a7697b7bcb916d2e33b9659b6a0d058
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-01-23 14:37:18 -0800

    If there are output messages, push the output message

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById('click').onclick = function() {
     let password = document.getElementById('password').value;
-    let outputMessages = ""
+    let outputMessages = "Password is invalid:"
     let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*']
     
     // Password Requirements
@@ -11,13 +11,20 @@ document.getElementById('click').onclick = function() {
     // At least 1 lowercase letter
     // At least 1 numeric digit
 
+    let counter = 0;
+
     if (password.length < 8) {
-        outputMessages += "The password must be at least 8 characters long.";
+        outputMessages += "<p>The password must be at least 8 characters long.</p>";
+        counter++;
     }
 
     if (!specialCharacters.some(char => password.includes(char))) {
         outputMessages += "The password must contain at least one special character (!, @, #, $).\n";
+        counter++;
     }
     
-    document.getElementById('result').innerHTML = outputMessages;
+    if (counter > 0) {
+        document.getElementById('result').innerHTML = outputMessages;
+    }
+    
 }
